c6c171b02573721de5340ba89c476649829bb810
Yoshida-Ryoma <ryouma_22s1100339@nnn.ed.jp>
2025-03-11 01:20:51 +0900

    second_commit

--- src/components/Menber.jsx
+++ src/components/Menber.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Menber.css'
 
 export default function Menber() {
   return (
@@ -6,7 +7,17 @@ export default function Menber() {
       <div className="menber block" id='menber'>
         <p className="main__font"><b>Menber</b></p>
         <div>
-          <div></div>
+          <div className='item'>
+            <button className='main__font'>
+              <p><b>吉田陵真</b></p>
+              <ul>
+                <li className="sub__font"><b>name:</b></li>
+                <li className="sub__font"><b>post:</b></li>
+                <li className="sub__font"><b>skill:</b></li>
+                <li className="sub__font"><b>activity:</b></li>
+              </ul>
+            </button>
+          </div>
           <div></div>
           <div></div>
         </div>
